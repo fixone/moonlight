46c098d962fc402f5b13cec5dcc1d630c5c57850
Felix <fixone@gmail.com>
2018-04-17 17:15:46 +0300

    save to local db

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 const config = require('./config')
 const ln = require('./lightning')
+const db = require('knex')(config.knex)
+
 const debug = require('debug')('app:api')
 const nanoid = require('nanoid')
 
@@ -18,8 +20,17 @@ router.post('/invoice',(req,res) => {
             let label = req.body.label || nanoid()
             let description = req.body.description || ("Lightning Payment order "+label)
             ln.client.call("invoice",[amount,label,description])
-            .then(rx => res.status(200).json(Object.assign({},config.errors['OK'],{id:label,description:description},rx)))
-            .catch(e => res.status(500).json(Object.assign({errorCode:e.error.code,errorMessage:e.error.message},e)))
+            .then(rx => {
+                res.status(200).json(Object.assign({},config.errors['OK'],{id:label,description:description},rx))
+                db('requests')
+                .insert({result:JSON.stringify(rx),id:label,'url':req.url,payment_hash:rx.payment_hash,expires_at:rx.expires_at,bolt11:rx.bolt11,type:config.types.INVOICE})
+                .then(x => debug("response %s saved %o",label,x))
+                .catch(ee => debug("error on insert %o",ee))
+            })
+            .catch(e => {
+                debug("error calling 'invoice' %o",e)
+                res.status(500).json(Object.assign( {errorCode:e.error.code,errorMessage:e.error.message},e))}
+            )
 
         } else {
             res.status(500).json(config.errors['INVOICE_AMOUNT'])
@@ -34,14 +45,21 @@ router.post('/pay', (req,res) => {
         if(req.body.bolt11 != null && req.body.bolt11.length > 0) {
             ln.client.call("decodepay",[req.body.bolt11])
             .then(rx => {
+                let id = nanoid()
                 debug("decodepay %o",rx)
+                db('requests')
+                    .insert({result:JSON.stringify(rx),id:id,'url':req.url,payment_hash:rx.payment_hash,expires_at:(rx.created_at+rx.expiry),bolt11:req.body.bolt11,type:config.types.PAYMENT})
+                    .then(x => debug("response %s saved %o",id,x))
+                    .catch(ee => debug("error on insert %o",ee))
                 ln.client.call("pay",[req.body.bolt11, req.body.msatoshi,
                     req.body.description, req.body.riskfactor || 1.0, req.body.maxfeepercent || 1.0,config.defaultTimeout])
                 .then( prx => {
+                    let rsp = Object.assign({id:id},config.errors['OK'],rx,prx)
+                    res.status(200).json(rsp)
                     
-                    res.status(200).json(Object.assign({},config.errors['OK'],rx,prx))
+
                 })
-                .catch(e => res.status(500).json(Object.assign({errorCode:e.error.code,errorMessage:e.error.message},rx,e)))
+                .catch(e => res.status(500).json(Object.assign({id:id,errorCode:e.error.code,errorMessage:e.error.message},rx,e)))
             })
             .catch(e => res.status(500).json(Object.assign({errorCode:e.error.code,errorMessage:e.error.message},e)))
         } else {
--- lightning.js
+++ lightning.js
@@ -3,6 +3,12 @@ const debug = require('debug')('app:rpc')
 const lnPath = config.lnPath || process.env.LN_PATH || require('path').join(require('os').homedir(), '.lightning')
 const LightningClient = require('lightning-client')
 const lnClient = new LightningClient(lnPath)
-lnClient.client.on('connect',_=>debug("connected to RPC socket"))
+lnClient.client.on('connect',_=> {
+    debug("connected to RPC socket")
+    //while we're here let's get rid of the old invoices
+    lnClient.call('delexpiredinvoice',[])
+    .then( r => debug("deleted old invoices %o",r))
+    .catch( e => debug("error in delete %o",e))
+})
 
 exports.client = lnClient
